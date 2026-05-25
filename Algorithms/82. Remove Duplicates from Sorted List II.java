5e0c666ce9397aedd8b365a4b6067c9a76226bdf
Jin <jinhyo.dev@gmail.com>
2026-05-25 13:48:44 +0900

    Add 82

---
+++ Algorithms/82. Remove Duplicates from Sorted List II.java
@@ -0,0 +1,19 @@
+public class Solution {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int val = cur.next.val;
+                while (cur.next != null && cur.next.val == val) cur.next = cur.next.next;
+            } else cur = cur.next;
+        }
+
+        return dummy.next;
+    }
+
+}
