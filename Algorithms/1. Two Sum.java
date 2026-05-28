4dfda9b405d4f209493cf1a53dcba5065f901922
Jin <jinhyo.dev@gmail.com>
2026-05-28 14:01:10 +0900

    Add 1

--- Algorithms/1. Two Sum.java
+++ Algorithms/1. Two Sum.java
@@ -7,7 +7,7 @@ public class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; ++i) {
             Integer v = map.get(nums[i]);
-            if (map.get(nums[i]) != null) return new int[]{v, i};
+            if (v != null) return new int[]{v, i};
 
             map.put(target - nums[i], i);
         }
