e8a8a61272fd28df71ec472750758a4166e31f9d
Jin <jinhyo.dev@gmail.com>
2026-06-15 19:23:18 +0900

    Add 144

--- Algorithms/144. Binary Tree Preorder Traversal.java
+++ Algorithms/144. Binary Tree Preorder Traversal.java
@@ -5,9 +5,8 @@ public class Solution {
 
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        if (root == null) return list;
-
         dfs(root, list);
+
         return list;
     }
 
