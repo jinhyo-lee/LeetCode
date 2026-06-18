5d714e48aabf726816f4fc00302522ad01e05d71
Jin <jinhyo.dev@gmail.com>
2026-06-18 14:22:32 +0900

    Add 171

--- Algorithms/171. Excel Sheet Column Number.java
+++ Algorithms/171. Excel Sheet Column Number.java
@@ -2,7 +2,7 @@ public class Solution {
 
     public int titleToNumber(String columnTitle) {
         int n = 0;
-        for (char c : columnTitle.toCharArray()) n = n * 26 + c - 'A' + 1;
+        for (char c : columnTitle.toCharArray()) n = n * 26 + c - 64;
 
         return n;
     }
