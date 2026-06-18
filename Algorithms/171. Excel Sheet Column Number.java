b272560b3e3045a0f128d79775ba5b8eddabfb4f
Jin <jinhyo.dev@gmail.com>
2026-06-18 14:19:33 +0900

    Add 171

---
+++ Algorithms/171. Excel Sheet Column Number.java
@@ -0,0 +1,10 @@
+public class Solution {
+
+    public int titleToNumber(String columnTitle) {
+        int n = 0;
+        for (char c : columnTitle.toCharArray()) n = n * 26 + c - 'A' + 1;
+
+        return n;
+    }
+
+}
