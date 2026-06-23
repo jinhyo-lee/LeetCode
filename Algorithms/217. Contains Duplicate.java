2db772296a0ca1acbccc42e74b1902a0074715e3
Jin <jinhyo.dev@gmail.com>
2026-06-23 13:24:03 +0900

    Add 217

---
+++ Algorithms/217. Contains Duplicate.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) if (!set.add(i)) return true;
+
+        return false;
+    }
+
+}
