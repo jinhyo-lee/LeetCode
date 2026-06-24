f50dc2067536be7e167482cf8db86ff5934ef460
Jin <jinhyo.dev@gmail.com>
2026-06-24 14:38:12 +0900

    Add 219

---
+++ Algorithms/219. Contains Duplicate II.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.add(nums[i])) return true;
+            if (set.size() > k) set.remove(nums[i - k]);
+        }
+
+        return false;
+    }
+
+}
