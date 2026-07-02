160e43525599827026ebc299ab3ce0d9d7aef91a
Jin <jinhyo.dev@gmail.com>
2026-07-02 10:35:23 +0900

    Add 232

---
+++ Algorithms/232. Implement Queue using Stacks.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class MyQueue {
+
+    Stack<Integer> in, out;
+
+    public MyQueue() {
+        in = new Stack<>();
+        out = new Stack<>();
+    }
+
+    public void push(int x) {
+        in.push(x);
+    }
+
+    public int pop() {
+        peek();
+        return out.pop();
+    }
+
+    public int peek() {
+        if (out.isEmpty()) while (!in.isEmpty()) out.push(in.pop());
+        return out.peek();
+    }
+
+    public boolean empty() {
+        return in.isEmpty() && out.isEmpty();
+    }
+
+}
