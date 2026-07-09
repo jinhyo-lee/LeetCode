ccaac06b32b41dc444f921e962fc3257a865ac2b
Jin <jinhyo.dev@gmail.com>
2026-07-09 09:40:21 +0900

    Add 268

---
+++ Algorithms/268. Missing Number.java
@@ -0,0 +1,10 @@
+public class Solution {
+
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) sum += i - nums[i];
+
+        return sum + nums.length;
+    }
+
+}
