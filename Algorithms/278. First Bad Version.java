e761f0f591c508ad968b8db30fc5b74fa5ec5cbf
Jin <jinhyo.dev@gmail.com>
2026-07-10 10:07:02 +0900

    Add 278

---
+++ Algorithms/278. First Bad Version.java
@@ -0,0 +1,14 @@
+public class Solution extends VersionControl {
+
+    public int firstBadVersion(int n) {
+        int s = 1, e = n;
+        while (s <= e) {
+            int m = s + (e - s) / 2;
+            if (isBadVersion(m)) e = m - 1;
+            else s = m + 1;
+        }
+
+        return s;
+    }
+
+}
