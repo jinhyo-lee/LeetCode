464dad8244c906a94d628c1c9b74aed2e604d1ad
Jin <jinhyo.dev@gmail.com>
2026-07-14 09:11:57 +0900

    Add 303

---
+++ Algorithms/303. Range Sum Query - Immutable.java
@@ -0,0 +1,16 @@
+public class NumArray {
+
+    int[] arr;
+
+    public NumArray(int[] nums) {
+        arr = new int[nums.length];
+        arr[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) arr[i] = arr[i - 1] + nums[i];
+    }
+
+    public int sumRange(int left, int right) {
+        if (left == 0) return arr[right];
+        return arr[right] - arr[left - 1];
+    }
+
+}
