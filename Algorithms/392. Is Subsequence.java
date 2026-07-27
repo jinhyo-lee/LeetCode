957d28b2292b5c7f35203b7dc32488bb4d38d65f
Jin <jinhyo.dev@gmail.com>
2026-07-27 09:34:22 +0900

    Add 392

---
+++ Algorithms/392. Is Subsequence.java
@@ -0,0 +1,11 @@
+public class Solution {
+
+    public boolean isSubsequence(String s, String t) {
+        if (s.isEmpty()) return true;
+        int i = 0;
+        for (char c : t.toCharArray()) if (c == s.charAt(i) && ++i == s.length()) return true;
+
+        return false;
+    }
+
+}
