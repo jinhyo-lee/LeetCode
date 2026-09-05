ac6cb6ec25bf9f806198b18298b5c39a33923a9e
Jin <jinhyo.dev@gmail.com>
2026-09-05 12:19:18 +0900

    Add 575

---
+++ Algorithms/575. Distribute Candies.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+    public int distributeCandies(int[] candyType) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : candyType) set.add(i);
+
+        return Math.min(set.size(), candyType.length / 2);
+    }
+
+}
