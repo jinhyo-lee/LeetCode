2604448e0a21b4a960bc45731953e61814df00e7
Jin <jinhyo.dev@gmail.com>
2026-09-22 14:41:10 +0900

    Add 680

---
+++ Algorithms/680. Valid Palindrome II.java
@@ -0,0 +1,16 @@
+public class Solution {
+
+    public boolean validPalindrome(String s) {
+        int l = -1, r = s.length();
+        while (++l < --r) if (s.charAt(l) != s.charAt(r)) return isPalindrome(s, l + 1, r) || isPalindrome(s, l, r - 1);
+
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int l, int r) {
+        while (l < r) if (s.charAt(l++) != s.charAt(r--)) return false;
+
+        return true;
+    }
+
+}
